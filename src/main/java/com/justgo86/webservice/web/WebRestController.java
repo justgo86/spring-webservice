8b99fc304fc6ad38e5a70a5e54bfe4400ec12866
justgo86-PC\justgo86 <justgo86@naver.com>
2019-03-14 00:26:39 +0900

    2강까지 마무리

--- src/main/java/com/justgo86/webservice/web/WebRestController.java
+++ src/main/java/com/justgo86/webservice/web/WebRestController.java
@@ -1,13 +1,26 @@
 package com.justgo86.webservice.web;
 
+import com.justgo86.webservice.domain.posts.PostsRepository;
+import com.justgo86.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello(){
         return "HelloWorld!!!";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 }
---
+++ src/test/java/com/justgo86/webservice/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,71 @@
+package com.justgo86.webservice.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        /**
+         * 이후 테스트 코드에 영향을 끼치지 않기 위해
+         * 테스트 메소드가 끝날때 마다 리포지도리 전체를 비우는 코드
+        **/
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        postsRepository.save(Posts.builder()
+                .title("test posts")
+                .content("test contents")
+                .author("justgo86@naver.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(), is("test posts"));
+        assertThat(posts.getContent(), is("test contents"));
+    }
+
+    @Test
+    public void BaseTimeEntity_regist(){
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("justgo86@naver.com")
+                .build()
+        );
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertTrue(posts.getCreatedDate().isAfter(now));
+        assertTrue(posts.getModifiedDate().isAfter(now));
+
+    }
+}
